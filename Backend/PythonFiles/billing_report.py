335b9b74f64a27160456f7ae77199152aab249e1
SureshKumar G <75471140+Suresh7375@users.noreply.github.com>
2025-04-10 22:36:43 +0530

    Update billing_report.py
    Python billing Report UI Improved

--- Backend/PythonFiles/billing_report.py
+++ Backend/PythonFiles/billing_report.py
@@ -52,7 +52,7 @@ def create_invoice(output_path):
 
     title_style = ParagraphStyle(name='Title', fontSize=16, alignment=TA_CENTER, textColor=BRAND_PRIMARY)
     header_style = ParagraphStyle(name='Header', fontSize=10, textColor=BRAND_PRIMARY)
-    normal_style = ParagraphStyle(name='Normal', fontSize=8)
+    normal_style = ParagraphStyle(name='Normal', fontSize=8, alignment=TA_RIGHT)
 
     elements = []
 
@@ -60,14 +60,30 @@ def create_invoice(output_path):
     elements.append(Spacer(1, 8))
 
     logo_path = "company_logo.png"
-    logo = Image(logo_path, width=2.5 * cm, height=2.5 * cm) if os.path.exists(logo_path) else Spacer(1, 2.5 * cm)
+    if os.path.exists(logo_path):
+        logo = Image(logo_path)
+        logo._restrictSize(2.2 * cm, 2.2 * cm)  # Maintain aspect ratio within given box
+    else:
+        logo = Spacer(1, 2.2 * cm)
 
+    company_name_para = Paragraph(
+        """<para align=left>
+            <font name="Helvetica-Bold" size="12" color="#2C3E50">Paramount Clothing</font><br/>
+            <font name="Helvetica" size="8" color="#7F8C8D">Quality Apparel Exporters</font>
+        </para>""",
+        styles["Normal"]
+    )
+
+    invoice_details = Paragraph("Invoice #: PC-0001<br/>Date: 09.04.2025<br/>Our Ref: PC-0425-PI-01<br/>Your Ref: PO# 20250409", normal_style)
     company_info = [[
         logo,
-        Paragraph("<b>Paramount Clothing</b>", header_style),
-        Paragraph("Invoice #: PC-0001<br/>Date: 09.04.2025<br/>Our Ref: PC-0425-PI-01<br/>Your Ref: PO# 20250409", normal_style)
+        company_name_para,
+        invoice_details
     ]]
     company_table = Table(company_info, colWidths=[2.5 * cm, 6.2 * cm, 10.0 * cm])
+    company_table.setStyle(TableStyle([
+        ('ALIGN', (2, 0), (2, 0), 'RIGHT')
+    ]))
     elements.append(company_table)
     elements.append(Spacer(1, 8))
 
@@ -85,7 +101,8 @@ def create_invoice(output_path):
         ["Address:", "#01-04, BLOCK 219"],
         ["Area:", "PARKLANDS, SERANGOON"],
         ["City:", "SINGAPORE"],
-        ["PIN:", "315602"]
+        ["PIN:", "315602"],
+        ["", ""]
     ]
 
     bill_to_table = Table(bill_to_data, colWidths=[3.2 * cm, 9.3 * cm])
@@ -102,7 +119,7 @@ def create_invoice(output_path):
     contact_table = Table([
         [Paragraph("<b>Bill To</b>", header_style), Paragraph("<b>Ship To</b>", header_style)],
         [bill_to_table, ship_to_table]
-    ], colWidths=[9.55 * cm, 9.55 * cm])
+    ], colWidths=[9.95 * cm, 9.95 * cm])
     contact_table.setStyle(TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), BRAND_LIGHT),
         ('BOX', (0, 0), (-1, -1), 0.3, colors.grey),
@@ -138,52 +155,91 @@ def create_invoice(output_path):
     elements.append(Spacer(1, 8))
 
     summary_data = [
-        ["Sub Total", "25,321.00"],
-        ["GST @ 7%", "1,772.47"],
-        ["Total", "27,093.47"]
+        [Paragraph("Sub Total", normal_style), Paragraph("25,321.00", normal_style)],
+        [Paragraph("GST @ 7%", normal_style), Paragraph("1,772.47", normal_style)],
+        [Paragraph("<b>Total</b>", ParagraphStyle('TotalBold', parent=normal_style, fontName='Helvetica-Bold')),
+         Paragraph("<b>27,093.47</b>",
+                   ParagraphStyle('TotalBoldRight', parent=normal_style, fontName='Helvetica-Bold'))]
     ]
-    summary_table = Table(summary_data, colWidths=[15.5 * cm, 3.6 * cm])
+
+    summary_table = Table(summary_data, colWidths=[15.0 * cm, 4.1 * cm])
     summary_table.setStyle(TableStyle([
-        ('FONTNAME', (0, 0), (-1, -2), 'Helvetica'),
-        ('FONTNAME', (0, 2), (-1, 2), 'Helvetica-Bold'),
-        ('FONTSIZE', (0, 0), (-1, -1), 8),
-        ('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
+        ('ALIGN', (0, 0), (0, -1), 'RIGHT'),
+        ('ALIGN', (1, 0), (1, -1), 'RIGHT'),
+        ('BACKGROUND', (0, -1), (-1, -1), BRAND_LIGHT),
+        ('LINEABOVE', (0, -1), (-1, -1), 0.5, colors.grey),
+        ('LINEBELOW', (0, -1), (-1, -1), 0.5, colors.grey),
+        ('TOPPADDING', (0, 0), (-1, -1), 4),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 4),
         ('RIGHTPADDING', (0, 0), (-1, -1), 8),
-        ('LEFTPADDING', (0, 0), (-1, -1), 8),
-        ('BOTTOMPADDING', (0, 0), (-1, -1), 5),
-        ('TOPPADDING', (0, 0), (-1, -1), 5),
-        ('BACKGROUND', (0, 2), (-1, 2), BRAND_LIGHT),
-        ('LINEABOVE', (0, 2), (-1, 2), 0.5, colors.grey),
-        ('LINEBELOW', (0, 2), (-1, 2), 0.5, colors.grey)
+        ('LEFTPADDING', (0, 0), (-1, -1), 4)
     ]))
     elements.append(summary_table)
-    elements.append(Spacer(1, 8))
 
-    elements.append(Paragraph(
-        "Amount in Words: Singapore Dollars Twenty Seven Thousand Ninety Three And Forty Seven Cents Only",
-        ParagraphStyle(name='Words', fontSize=7.5)))
+    # elements.append(Spacer(1, 4))
+    LEFT_INDENT_CM = 0.3 * cm
+
+    amount_words_para = Paragraph(
+        "<b>Amount in Words:</b><br/><font color='#2C3E50'>Singapore Dollars Twenty Seven Thousand Ninety Three And Forty Seven Cents Only</font>",
+        ParagraphStyle(
+            name='Words',
+            fontSize=8,
+            leading=10,
+            textColor=BRAND_SECONDARY,
+            alignment=TA_LEFT
+        )
+    )
+    amount_words_table = Table([[amount_words_para]], colWidths=[19.9 * cm])
+    amount_words_table.setStyle(TableStyle([
+        ('LEFTPADDING', (0, 0), (-1, -1), LEFT_INDENT_CM),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 0),
+    ]))
+    elements.append(Spacer(1, 4))
+    elements.append(amount_words_table)
 
     small_style = ParagraphStyle(name="Small", fontSize=7.5)
+
     bank_data = [
-        [Paragraph("<b>Account Name:</b>", small_style), Paragraph("Paramount Clothing", small_style),
-         Paragraph("<b>Bank Name:</b>", small_style), Paragraph("Indian Bank, Pallavaram", small_style)],
-        [Paragraph("<b>A/C No.:</b>", small_style), Paragraph("7678197857", small_style),
-         Paragraph("<b>SWIFT Code:</b>", small_style), Paragraph("IDIBINBBMEP", small_style)],
-        [Paragraph("<b>IFS Code:</b>", small_style), Paragraph("IDIB000P012", small_style),
-         Paragraph("", small_style), Paragraph("", small_style)]
+        [Paragraph("<b>Account Name:</b>", small_style), "Paramount Clothing",
+         Paragraph("<b>A/C No.:</b>", small_style), "7678197857"],
+        [Paragraph("<b>Bank Name:</b>", small_style), "Indian Bank, Pallavaram",
+         Paragraph("<b>SWIFT Code:</b>", small_style), "IDIBINBBMEP"],
+        [Paragraph("<b>IFS Code:</b>", small_style), "IDIB000P012", "", ""]
     ]
 
     bank_table = Table(bank_data, colWidths=[3.2 * cm, 6.5 * cm, 3.2 * cm, 6.5 * cm])
     bank_table.setStyle(TableStyle([
+        ('GRID', (0, 0), (-1, -1), 0.3, colors.lightgrey),
+        ('FONTNAME', (0, 0), (-1, -1), 'Helvetica'),
+        ('FONTSIZE', (0, 0), (-1, -1), 7.8),
         ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-        ('BOTTOMPADDING', (0, 0), (-1, -1), 2),
-        ('TOPPADDING', (0, 0), (-1, -1), 1),
-        ('LINEBELOW', (0, 0), (-1, -1), 0.1, colors.lightgrey)
+        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+        ('TOPPADDING', (0, 0), (-1, -1), 3),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 3),
+        ('LEFTPADDING', (0, 0), (-1, -1), 4),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 4),
     ]))
 
-    elements.append(Spacer(1, 8))
-    elements.append(Paragraph("Bank Details", ParagraphStyle(name='BankHeader', fontSize=8.5, textColor=BRAND_PRIMARY)))
+    bank_header = Paragraph(
+        "<b><u>Bank Details</u></b>",
+        ParagraphStyle(
+            name='BankHeader',
+            fontSize=8.5,
+            textColor=BRAND_PRIMARY,
+            alignment=TA_LEFT
+        )
+    )
+    bank_header_table = Table([[bank_header]], colWidths=[19.9 * cm])
+    bank_header_table.setStyle(TableStyle([
+        ('LEFTPADDING', (0, 0), (-1, -1), LEFT_INDENT_CM),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 0),
+    ]))
+    elements.append(Spacer(1, 6))
+    elements.append(bank_header_table)
+
+    # Existing bank table remains as is
     elements.append(bank_table)
+
     elements.append(Spacer(1, 6))
     elements.append(Paragraph("For Paramount Clothing<br/><br/><br/>Authorized Signatory",
                               ParagraphStyle(name='Sign', alignment=TA_RIGHT, fontSize=8)))
